ed5361fc39c053979fd4726c581492915200ef68
Marko Čepo <marko.cepo.vk@gmail.com>
2020-08-18 14:06:42 +0200

    frontend build

--- spa/src/plugins/social-auth.ts
+++ spa/src/plugins/social-auth.ts
@@ -6,7 +6,7 @@ Vue.use(VueSocialauth, {
   providers: {
     google: {
       clientId: '272921548264-ulgt9t470ppue02lr2n6a6n4su1rh42s.apps.googleusercontent.com',
-      redirectUri: 'http://localhost:8081' // Your client app URL
+      redirectUri: process.env.VUE_APP_GOOGLE_REDIRECT_URI// Your client app URL
     }
   }
 })
