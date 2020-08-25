a1351e4e70a17fa81cf2d260886d24e9421dbf2c
Marko Čepo <marko.cepo.vk@gmail.com>
2020-08-25 08:52:00 +0200

    FIX: mobile image not orientated properly

--- api/app/Models/Traits/HasImages.php
+++ api/app/Models/Traits/HasImages.php
@@ -3,7 +3,9 @@
 namespace App\Models\Traits;
 
 use App\Models\Image;
+use Intervention\Image\Facades\Image as InterventionImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 trait HasImages
 {
@@ -16,15 +18,23 @@ trait HasImages
     {
         $randomFolder = rand(0, config('filesystems.disks.images.number_of_folders') - 1);
 
-        $filePath = $request->file('image')
-            ->store($randomFolder, 'images');
+        $file = $request->file('image');
 
-            $size = getimagesize($request->file('image'));
+        $path = $file->hashName($randomFolder);
+
+        $image = InterventionImage::make($file);
+
+        // so that mobile images are displayed correctly
+        $image->orientate();
+
+        Storage::put('images/'.$path, (string) $image->encode());
+
+        $size = [$image->width(), $image->height()];
 
         $image = $this->images()
             ->create([
-                'file_path' => $filePath,
-                'size' => [$size[0], $size[1]]
+                'file_path' => $path,
+                'size' => $size
             ]);
 
         return $image;
