16239f9eea4901da8785d89f170c4d22f720c09e
Marko Čepo <marko.cepo.vk@gmail.com>
2020-09-21 13:09:37 +0200

    removed the auto service worker replace

--- spa/src/service-worker.js
+++ spa/src/service-worker.js
@@ -64,11 +64,3 @@ workbox.routing.registerRoute(matchCb, handleCb, 'POST')
 workbox.routing.registerRoute(matchCb, handleCb, 'PUT')
 
 workbox.routing.registerRoute(matchCb, handleCb, 'DELETE')
-
-self.addEventListener('install', () => {
-  self.skipWaiting()
-})
-
-self.addEventListener('activate', () => {
-  clients.claim()
-})
