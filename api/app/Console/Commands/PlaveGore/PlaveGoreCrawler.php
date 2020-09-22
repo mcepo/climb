cc9357f6670de55fef9a64f62c5dbbf5d06d4d10
Marko Čepo <marko.cepo.vk@gmail.com>
2020-09-22 14:47:05 +0200

    plave gore checkpoint

--- api/app/Console/Commands/PlaveGore/PlaveGoreCrawler.php
+++ api/app/Console/Commands/PlaveGore/PlaveGoreCrawler.php
@@ -54,11 +54,15 @@ class PlaveGoreCrawler extends Command
 
     private $gradeParser;
 
+    private $_country;
+
     public function __construct()
     {
         parent::__construct();
 
         $this->gradeParser = new GradeParser();
+
+        $this->_country = Area::where('name', 'ilike', 'Croatia')->first();
 
         //  \DB::listen(function ($query) {
         //      dump(
@@ -86,10 +90,12 @@ class PlaveGoreCrawler extends Command
 
         for($i=1;$i <= self::$MOUNTAIN_COUNT; $i++) {
 
-            $page = $this->_getPage('http://plave-gore.com/mt?id='.$i);
+            $link = 'http://plave-gore.com/mt?id='.$i;
+
+            $page = $this->_getPage($link);
 
             if ($page == null) {
-                echo ' **** http://plave-gore.com/mt?id='.$i. ' is null';
+                echo ' **** '. $link . ' is null';
                 exit;
             }
 
@@ -101,53 +107,16 @@ class PlaveGoreCrawler extends Command
         return;
 
         for($i=1;$i <= self::$CRAG_COUNT; $i++) {
-            $page = $this->_getPage('http://plave-gore.com/cr?id='.$i);
+
+            $link = 'http://plave-gore.com/cr?id='.$i;
+
+            $page = $this->_getPage($link);
 
             if ($page == null) {
-                echo ' **** http://plave-gore.com/cr?id='.$i. ' is null';
+                echo ' **** '.$link. ' is null';
                 exit;
             }
         }
-
-        // $countries->each(
-        //         function ($tr, $i) {
-        //             $td = $tr->children();
-
-        //             $country = $td->eq(0)->text();
-
-        //             //check if country has a english name in list
-        //             if (!isset(self::$replace_country[$country])) {
-        //                 echo "Country $country  not listed in replace array \n";
-
-        //                 return;
-        //             }
-        //             // check if country in pattern for parsing
-        //             if (!preg_match("/$this->_pattern/", self::$replace_country[$country])) {
-        //                 echo "Skipping country $country \n";
-
-        //                 return ;
-        //             }
-        //             echo 'Parsing countery ' . $country . "\n";
-
-        //             // get all pages for parsing
-        //             $pages = [
-        //                 $td->eq(2)->filter('a'), // peak list
-        //                 $td->eq(3)->filter('a'), // crags list
-        //                 $td->eq(5)->filter('a'), // boldering areas
-        //                 $td->eq(7)->filter('a') // ice climbing valleys
-        //             ];
-       
-        //             foreach ($pages as $page) {
-        //                 if ($page->count() === 1) {
-        //                     // parse page
-        //                     $this->_getFromLink(
-        //                         $this->base_url
-        //                         . $page->attr('href')
-        //                     );
-        //                 }
-        //             }
-        //         }
-        //     );
 
         $this->_runtime = microtime(true) - $this->_runtime;
 
@@ -155,48 +124,59 @@ class PlaveGoreCrawler extends Command
     }
 
     private function parseMountain($page) {
-        
+
+        $areaName = $page->filter('div#page-title-content > h2')->eq(0)->text();
+
+        // type id for area => 1
+        $area = $this->_getArea($this->_country, $areaName, 1);
+
+        $cragLinks = $page->filter('.col-container > .one-third > dl#quick_facts > dt > a');
+
+        $cragLinks->each(function($a) use ($area) {
+
+            $page = $this->_getPage($a->attr('href'));
+
+            if ($page == null) {
+                echo ' **** '.$a->attr('href'). ' is null';
+                exit;
+            }
+
+            $this->_parseCrag($area, $page);
+
+        });
     }
 
-    private function _getFromLink($link)
+    private function _getArea($parent, $name, $typeId) 
     {
-        $page = $this->_getPage($link);
+        $areas = Area::where('name', 'ilike', $name)->get();
 
-        if ($page == null) {
-            return;
+        if($areas->count() ==  0) {
+
+            echo $name . " -> new area\n";
+
+            $area = new Area([
+                'name' => $name,
+                'parent_id' => $parent->id,
+                'type_id' => $typeId
+            ]);
+
+       //     $area->save();
+
+        } else if ($areas->count() >  1) {
+            echo $name . " -> multiple results\n";
         }
 
-        // get the table with useful data from the page
-        $areas = $page->filter('.fmtTable > tr, #wallTable > tr');
+        if(!isset($area)) {
+            $area = $areas[0];
+        }
 
-        // foreach row, if a link exists in row follow that link
-        // to new page
-        $areas->each(
-                function ($tr) {
-                    // rows with this class are to be skipped
-                    if ($tr->attr('class') !== 'sectorRow expandable') {
-                        // getting links from row
-                        $a = $tr->filter('a');
-
-                        // if there is only one link
-                        if (($a->count()) === 1) {
-                            // get area type that the link will lead to
-                            $type = $this->_getAreaType($a);
-
-                            // call aproppriate method for parsing
-                            if ($type === 'crag') {
-                                $this->_parseCrag($this->base_url . $a->attr('href'));
-                            } else {
-                                $this->_getFromLink($this->base_url . $a->attr('href'));
-                            }
-                        }
-                    }
-                }
-            );
+        return $area;
     }
 
     private function _getPage($link)
     {
+        echo 'Crawling -> ' . $link . "\n";
+
         if (Link::where('href', $link)->first()) {
             echo "SKIPPING (link exists - DIDN'T OPEN LINK) : $link \n";
 
@@ -217,180 +197,33 @@ class PlaveGoreCrawler extends Command
         return $page;
     }
 
-    private function _parseCrag($link)
+    private function _parseCrag($parent, $page)
     {
-        $page = $this->_getPage($link);
 
-        if ($page == null) {
-            return;
-        }
+        $name = $page->filter('div.full-width > h2')->eq(0)->text();
 
-        [$name, $parent] = $this->_parseBreadcrumbs($page->filter('#breadcrumbs > p'));
+        // type_id for crag => 6
+        $crag = $this->_getArea($parent, $name, 6);
 
-        $parent = $this->_storeArea($parent, 'crag', $link, $name);
-        $grandParent = $parent;
+        // get location, store in database
 
-        $routes = $page->filter('#routeTable > tbody > tr');
+        $routeTableRows = $page->filter('table.datatable > tbody > tr');
 
-        $routes->each(
-                function ($tr, $i) use ($grandParent, &$parent, $link) {
-                    if ($tr->attr('class') == 'sectorRow expandable') {
-                        // this is a sector so store it as area
-                        $parent = $this->_storeArea($grandParent, 'sector', $link, trim($tr->text()));
-                    } else {
-                        // this  is an actual route
-                        $td = $tr->filter('td');
-                        $href = $this->base_url . $td->eq(0)->filter('a')->eq(0)->attr('href');
+        $routeTableRows->each(function($tr) use ($crag){
 
-                        $length = (int)preg_replace('/[^0-9]/', '', $td->eq(2)->text());
-                        $grade = trim($td->eq(1)->text());
+            $tds = $tr->children();
 
-                        [$grades, $pitches] = $this->gradeParser->parse($grade);
+            $name = $tds->eq(1)->text();
+            $grade = $tds->eq(2)->text();
+            $length = $tds->eq(3)->text();
 
-                        $route = [
-                            'name' => trim($td->eq(0)->filter('a')->eq(0)->text()),
-                            'length' => $length,
-                            'grades' => $grades
-                        ];
+            // multipitch 0 ; singlepitch 1
+            $type_id = $length > 50 ? 0 : 1;
 
-                        $routeModel = $this->_storeRoute($route, $parent, $pitches);
-                        $this->_storeLink($routeModel, $href );
-                    }
-                }
-            );
-    }
+             dump($name, $grade, $length, $type_id);
+        });
 
-    private function _parseBreadcrumbs($p)
-    {
-        $temp = explode(' >> ', $p->text());
-        $name = end($temp);
-
-        $parent = null;
-
-        $p->eq(0)->filter('a')
-                ->each(
-                    function ($a, $i) use (&$parent) {
-                        if ($i < 3) {
-                            return;
-                        }
-
-                        $name = $a->text();
-                        $link = $this->base_url . $a->attr('href');
-
-                        $type = $this->_getAreaType($a);
-
-                        if ($type == 'type') {
-                            $type = 'country';
-                            $name = self::$replace_country[$name];
-                        }
-
-                        $parent = $this->_storeArea($parent, $type, $link, $name);
-                    }
-                );
-
-        return [$name, $parent];
-    }
-
-    private function _storeArea($parent, $type, $href, $name)
-    {
-        // checking if his link was already visited
-        $link = Link::where('href', $href)->first();
-
-        // if the area is a sector the don't check that it exists
-        // because a lot of sectors has short names and there is a lot of duplicates
-        if (isset($link->linked_id) && $type != 'sector') {
-            echo "SKIPPING link exists: $href -> $name -> $type\n";
-            return Area::find($link->linked_id);
-        }
-
-        // check if the area is already stored by checking area name and parent id
-        if ($parent == null) {
-            $area = Area::where(['name' => $name, 'parent_id' => null])->first();
-        } else {
-            $area = Area::where(['name' => $name, 'parent_id' => $parent->id])->first();
-        }
-
-        if (isset($area->id)) {
-            echo 'SKIPPING: area exists!';
-
-            return $area;
-        }
-
-        // if the area is a peak get altitude from area name
-        if ($type === 'peak') {
-            // get max altitude from peak name
-            [$name, $maxAltitude] = $this->_parseMaxAltitude($name);
-        }
-
-        // get mountain side from area name
-        $name = $this->_parseMountainSide($name);
-        $orientation = $this->_parseOrientation($name);
-
-        // if i got orientation from area name then it is a mountain side
-        if (!is_null($orientation)) {
-            $type = 'mountain_side';
-        }
-
-        // get id of area type
-        $areaType = $this->_getAreaTypeId($type);
-
-        if ($areaType === false) {
-            // id must exist if it doesn't exist then this is an error
-            throw new \Exception("Area type {$type} not found!\n");
-        }
-
-        $area = [
-            'name' => $name,
-            'max_altitude' => isset($maxAltitude) ? $maxAltitude : null,
-            'type_id' => $areaType,
-            'orientation' => $orientation
-        ];
-
-        if ($parent) {
-            $area['parent_id'] = $parent->id;
-        }
-
-        $area = new Area($area);
-        $area->save();
-
-        $this->_storeLink($area, $href);
-
-        echo '*** AREA: ' . $area->toJson() . "\n";
-
-        return $area;
-    }
-
-    private function _storeRoute($route, $area, $pitches = [])
-    {
-        if ($route['length'] == 0) {
-            $route['length'] = null;
-        }
-        $routeType = $this->_getRouteTypeId((!empty($pitches) || $route['length'] > 40) ? 'multipitch' : 'singlepitch');
-
-        if ($routeType === false) {
-            throw new \Exception("No route type {$routeType}!\n");
-        }
-
-        $route['type_id'] = $routeType;
-        $route['area_id'] = $area->id;
-        $grades = $route['grades'];
-
-        $route = new Route($route);
-        $route->save();
-
-        $route->saveGrades($grades);
-
-        foreach ($pitches as $key => $value) {
-            [$grades] = $this->gradeParser->parse($value);
-            $pitch = $route->pitches()->create([
-                'length' => null,
-            ]);
-            $pitch->saveGrades($grades);
-        }
-
-        echo '** ROUTE: ' . $route->toJson() . "\n";
-
-        return $route;
+        exit;
     }
 
     private function _storeLink($model, $href)
@@ -398,69 +231,12 @@ class PlaveGoreCrawler extends Command
         $link = $model->links()
                 ->create(
                     [
-                        'name' => 'Plezanje.net',
+                        'name' => 'Plave gore',
                         'href' => $href
                     ]
                 );
 
         echo '** LINK: ' . $link->toJson() . "\n";
-    }
-
-    private function _parseMountainSide($name)
-    {
-        foreach (self::$mountainSides as $sloMountainSide => $engMountainSide) {
-            if (strpos($name, ', ' . $sloMountainSide) !== false) {
-                return $engMountainSide;
-            }
-        }
-
-        return $name;
-    }
-
-    private function _parseOrientation($name)
-    {
-        foreach (self::$orientations as $orientation => $deg) {
-            if (strcmp($name, $orientation) == 0) {
-                return $deg;
-            }
-        }
-
-        return null;
-    }
-
-    private function _parseMaxAltitude($name)
-    {
-        $tmp = preg_split("/\(|\)/", $name);
-
-        if (count($tmp) == 1) {
-            return [
-                $name,
-                null
-            ];
-        } else {
-            $max_altitude = (int) filter_var($tmp[1], FILTER_SANITIZE_NUMBER_INT);
-            if ($max_altitude == 0) {
-                $max_altitude = null;
-            }
-
-            return [
-                trim($tmp[0]),
-                $max_altitude
-            ];
-        }
-    }
-
-    private function _getAreaType($a)
-    {
-        return (preg_split('/[\\?=]/', $a->attr('href')))[1];
-    }
-
-    private function _getRouteTypeId($type) {
-        return array_search($type, config('types.routes'));
-    }
-
-    private function _getAreaTypeId($type) {
-        return array_search($type, config('types.areas'));
     }
 
     private function _loginMe() {
