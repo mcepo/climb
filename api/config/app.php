d645a324d0ae2d91261fec2be8b1674bf6a79684
Marko Čepo <marko.cepo.vk@gmail.com>
2020-09-28 12:40:26 +0200

    removed clockwork has autodiscovered

--- api/config/app.php
+++ api/config/app.php
@@ -149,7 +149,6 @@ return [
          * Package Service Providers...
          */
 
-        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -208,7 +207,5 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        
-        'Clockwork' => Clockwork\Support\Laravel\Facade::class,
     ],
 ];
