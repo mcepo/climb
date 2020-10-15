78d2e72c462a55ed239f1343e113615c3c8ce191
Marko Čepo <marko.cepo.vk@gmail.com>
2020-10-15 19:25:26 +0200

    v-intersect above loading so i don't get glitches

--- spa/src/components/lists/RouteList.vue
+++ spa/src/components/lists/RouteList.vue
@@ -8,16 +8,16 @@
   </v-toolbar>
   <v-list two-line>
     <route-list-item v-for="route in routes" :key="route.id" :route='route'/>
+    <v-list-item>
+      <v-list-item-content>
+        <div v-intersect='loadOnIntersect'></div>
+      </v-list-item-content>
+    </v-list-item>
     <v-list-item v-if='loading'>
       <v-list-item-content>
         <v-progress-circular
           indeterminate
         ></v-progress-circular>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item>
-      <v-list-item-content>
-        <div v-intersect='loadOnIntersect'></div>
       </v-list-item-content>
     </v-list-item>
   </v-list>
