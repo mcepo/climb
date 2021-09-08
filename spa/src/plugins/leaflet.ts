ea1e5e13a14c5292c0bffba52308eaf048697e46
Marko Čepo <marko.cepo@marker.hr>
2021-09-08 14:19:33 +0200

    fix: 404 marker icon request

--- spa/src/plugins/leaflet.ts
+++ spa/src/plugins/leaflet.ts
@@ -1,10 +1,14 @@
 import { svgIconUrl } from '../utils/svg'
-import { Icon } from 'leaflet'
+import { BaseIconOptions, Icon, Marker } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-Icon.Default.prototype.options = {
+const _defaultIcon = {
   iconUrl: svgIconUrl('<path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><path d="M0 0h24v24H0z" fill="none"/>'),
   iconSize: [20, 32],
   iconAnchor: [10, 24],
   shadowUrl: undefined
-}
+} as BaseIconOptions
+
+Icon.Default.prototype.options = _defaultIcon
+
+Marker.prototype.options.icon = new Icon(_defaultIcon)
