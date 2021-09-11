c69dab32da01ccf8e2f8a5b11964b48e70bd1516
Marko <marko.cepo@marker>
2021-09-11 22:51:52 +0200

    build update

---
+++ api/public/assets/precache-manifest.3a26a5b1c85eb92151034464154ed722.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78c4f32a83d0b4b11c5",
+    "url": "/assets/css/admin.panel.a8e99c9b.css"
+  },
+  {
+    "revision": "a0ff44b76923f74c1efc",
+    "url": "/assets/css/admin.panel~area.form~moderator.form~route.filters~route.form.35a70ef6.css"
+  },
+  {
+    "revision": "1bca7219e648579ecc57",
+    "url": "/assets/css/app.6de2e16b.css"
+  },
+  {
+    "revision": "e2824778d6a50cedf17c",
+    "url": "/assets/css/area.form.09316a61.css"
+  },
+  {
+    "revision": "984ace6e2100d2248cd3",
+    "url": "/assets/css/area.form~pitch.form~route.filters~route.form.f4cd3df8.css"
+  },
+  {
+    "revision": "5cad11461e16e1ee492d",
+    "url": "/assets/css/chunk-vendors.c7199c41.css"
+  },
+  {
+    "revision": "988fbf43fad247da4d58",
+    "url": "/assets/css/image.upload.1cead4a4.css"
+  },
+  {
+    "revision": "f62bee79739d04afd31c",
+    "url": "/assets/css/moderator.form.d3de4bc2.css"
+  },
+  {
+    "revision": "67d2428818a3ce41b50f",
+    "url": "/assets/css/pitch.form.207437e9.css"
+  },
+  {
+    "revision": "c016a88ba27b9da82d03",
+    "url": "/assets/css/route.filters.09316a61.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/assets/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/assets/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/assets/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/assets/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "4300a1a5ca6fb58e85a83f2189f164e9",
+    "url": "/assets/index.html"
+  },
+  {
+    "revision": "c78c4f32a83d0b4b11c5",
+    "url": "/assets/js/admin.panel.d7c732d6.js"
+  },
+  {
+    "revision": "a0ff44b76923f74c1efc",
+    "url": "/assets/js/admin.panel~area.form~moderator.form~route.filters~route.form.628419d8.js"
+  },
+  {
+    "revision": "1bca7219e648579ecc57",
+    "url": "/assets/js/app.ad89b92b.js"
+  },
+  {
+    "revision": "e2824778d6a50cedf17c",
+    "url": "/assets/js/area.form.c8f5a35f.js"
+  },
+  {
+    "revision": "984ace6e2100d2248cd3",
+    "url": "/assets/js/area.form~pitch.form~route.filters~route.form.5b34b03a.js"
+  },
+  {
+    "revision": "2717a641bb4a4f9cef25",
+    "url": "/assets/js/change-password.form.d71c6465.js"
+  },
+  {
+    "revision": "5cad11461e16e1ee492d",
+    "url": "/assets/js/chunk-vendors.7547a7cf.js"
+  },
+  {
+    "revision": "988fbf43fad247da4d58",
+    "url": "/assets/js/image.upload.1d261626.js"
+  },
+  {
+    "revision": "36ec1216b4984dcb567a",
+    "url": "/assets/js/login.form.fdf3d024.js"
+  },
+  {
+    "revision": "f62bee79739d04afd31c",
+    "url": "/assets/js/moderator.form.83af82c1.js"
+  },
+  {
+    "revision": "67d2428818a3ce41b50f",
+    "url": "/assets/js/pitch.form.56ddc5be.js"
+  },
+  {
+    "revision": "3e013dd7ca094d5de8c6",
+    "url": "/assets/js/popup.service.e7658555.js"
+  },
+  {
+    "revision": "81d4bdf0d2df7b7ae659",
+    "url": "/assets/js/register.form.e33aed46.js"
+  },
+  {
+    "revision": "c016a88ba27b9da82d03",
+    "url": "/assets/js/route.filters.f5e33f54.js"
+  },
+  {
+    "revision": "b01d769f896b7bcdbc5b",
+    "url": "/assets/js/route.form.0f04b47d.js"
+  },
+  {
+    "revision": "372d76914e2e36cf8f8a23af440c030f",
+    "url": "/assets/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/assets/robots.txt"
+  }
+]);
--- api/public/service-worker.js
+++ api/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/assets/precache-manifest.c831eac907dc02cd35733e74b7fa943a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/assets/precache-manifest.3a26a5b1c85eb92151034464154ed722.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // this is an ugly solution but i needed it so the service worker will
 // detect the change in the root url '/', but also serve it once offline
