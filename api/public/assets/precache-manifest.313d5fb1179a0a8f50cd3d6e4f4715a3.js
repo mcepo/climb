34b05a6a5198797138bcbacf8568fa8856e5c6d9
Marko <marko.cepo@marker>
2021-11-13 19:17:32 +0100

    todo update

--- api/public/assets/precache-manifest.313d5fb1179a0a8f50cd3d6e4f4715a3.js
+++ api/public/assets/precache-manifest.313d5fb1179a0a8f50cd3d6e4f4715a3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "909812334dc201f18931",
+    "revision": "53fae2fcbc8dcd3f0186",
     "url": "/assets/css/admin.panel.2ab20800.css"
   },
   {
@@ -8,11 +8,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/assets/css/admin.panel~area.form~moderator.form~route.filters~route.form.f058a909.css"
   },
   {
-    "revision": "7217f8bae18820ffcda3",
+    "revision": "ffa6b38da4ea588e5015",
     "url": "/assets/css/app.fb0abc21.css"
   },
   {
-    "revision": "358c5f7274473f95ff05",
+    "revision": "23554e85946e6dc7ab21",
     "url": "/assets/css/area.form.09316a61.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/assets/css/chunk-vendors.4002c9eb.css"
   },
   {
-    "revision": "709c13d804ee961036c9",
+    "revision": "4c9b500052f35de9dc2c",
     "url": "/assets/css/image.upload.640da394.css"
   },
   {
@@ -32,15 +32,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/assets/css/leaflet-geoman.8c7a4794.css"
   },
   {
-    "revision": "9d2d7cdb6dbd10315166",
+    "revision": "141720f05d9a41ae2ae5",
     "url": "/assets/css/moderator.form.d3de4bc2.css"
   },
   {
-    "revision": "85d43bf7675c5724387c",
+    "revision": "e58bde14f73927174c97",
     "url": "/assets/css/pitch.form.207437e9.css"
   },
   {
-    "revision": "d81d53ccaaf137bd7ae2",
+    "revision": "d8a9dc9bc3db9c28b7f3",
     "url": "/assets/css/route.filters.09316a61.css"
   },
   {
@@ -60,32 +60,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/assets/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
-    "revision": "2eafb4eaf2bb61f7779f58a8498615b8",
+    "revision": "b0c0cdaa3b40cfc49ae7ef2e2922a3ee",
     "url": "/assets/index.html"
   },
   {
-    "revision": "909812334dc201f18931",
-    "url": "/assets/js/admin.panel.ce444211.js"
+    "revision": "53fae2fcbc8dcd3f0186",
+    "url": "/assets/js/admin.panel.f9ecd5f3.js"
   },
   {
     "revision": "5ec2b75f686dda5035f9",
     "url": "/assets/js/admin.panel~area.form~moderator.form~route.filters~route.form.9450cd7d.js"
   },
   {
-    "revision": "7217f8bae18820ffcda3",
-    "url": "/assets/js/app.a7585ad5.js"
+    "revision": "ffa6b38da4ea588e5015",
+    "url": "/assets/js/app.32f252e7.js"
   },
   {
-    "revision": "358c5f7274473f95ff05",
-    "url": "/assets/js/area.form.aeb2be9e.js"
+    "revision": "23554e85946e6dc7ab21",
+    "url": "/assets/js/area.form.a3e3d584.js"
   },
   {
     "revision": "37f2ab522ff975ba1b8d",
     "url": "/assets/js/area.form~pitch.form~route.filters~route.form.42e79346.js"
   },
   {
-    "revision": "d54a283e1704919ed9df",
-    "url": "/assets/js/change-password.form.2b6bba0d.js"
+    "revision": "e70914a5cd32e2a20003",
+    "url": "/assets/js/change-password.form.6329d1d6.js"
   },
   {
     "revision": "f3f3e66ab17077d8a78e",
@@ -108,52 +108,52 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/assets/js/chunk-vendors.ec4545da.js"
   },
   {
-    "revision": "2b5f9f476fbac63c1028",
-    "url": "/assets/js/image.map.e1867261.js"
+    "revision": "a574accd522511221d59",
+    "url": "/assets/js/image.map.4cc491b3.js"
   },
   {
-    "revision": "709c13d804ee961036c9",
-    "url": "/assets/js/image.upload.0bbace58.js"
+    "revision": "4c9b500052f35de9dc2c",
+    "url": "/assets/js/image.upload.f9ddf5c7.js"
   },
   {
     "revision": "ac380fad5131d53d8117",
     "url": "/assets/js/leaflet-geoman.6a8b4c6d.js"
   },
   {
-    "revision": "9caad5d141f33533c567",
-    "url": "/assets/js/leaflet.map.1d6363d0.js"
+    "revision": "83784ac993f192850240",
+    "url": "/assets/js/leaflet.map.282f5996.js"
   },
   {
-    "revision": "872c9ee176836384cfbf",
-    "url": "/assets/js/login.form.1d97b6d6.js"
+    "revision": "49516c4c7d8851de2270",
+    "url": "/assets/js/login.form.602e1d60.js"
   },
   {
-    "revision": "9d2d7cdb6dbd10315166",
-    "url": "/assets/js/moderator.form.057c5a29.js"
+    "revision": "141720f05d9a41ae2ae5",
+    "url": "/assets/js/moderator.form.33f7ad93.js"
   },
   {
-    "revision": "85d43bf7675c5724387c",
-    "url": "/assets/js/pitch.form.98b422f4.js"
+    "revision": "e58bde14f73927174c97",
+    "url": "/assets/js/pitch.form.f8f2d17e.js"
   },
   {
-    "revision": "87f0dfe5aab308432830",
-    "url": "/assets/js/popup.service.87743d68.js"
+    "revision": "6abd30e489f47c949e81",
+    "url": "/assets/js/popup.service.221c366b.js"
   },
   {
-    "revision": "929a9169d52b078f8ec3",
-    "url": "/assets/js/privacy.policy.88c5b93c.js"
+    "revision": "72ab4e96ed2390c88c16",
+    "url": "/assets/js/privacy.policy.b9c7c9b9.js"
   },
   {
-    "revision": "62fc88d4233ab42a29ff",
-    "url": "/assets/js/register.form.55f63fc1.js"
+    "revision": "fa5df9b8f6ccaca2b440",
+    "url": "/assets/js/register.form.662d2a69.js"
   },
   {
-    "revision": "d81d53ccaaf137bd7ae2",
-    "url": "/assets/js/route.filters.a6570f22.js"
+    "revision": "d8a9dc9bc3db9c28b7f3",
+    "url": "/assets/js/route.filters.ff2e2bce.js"
   },
   {
-    "revision": "11fb3797fbcba3425bb4",
-    "url": "/assets/js/route.form.fb65b200.js"
+    "revision": "40dfee3b2e4142484ea7",
+    "url": "/assets/js/route.form.726b00c4.js"
   },
   {
     "revision": "372d76914e2e36cf8f8a23af440c030f",
--- api/public/service-worker.js
+++ api/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/assets/precache-manifest.118a541d1e1cdf51d3aefa0f8f7cd0c0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/assets/precache-manifest.313d5fb1179a0a8f50cd3d6e4f4715a3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // this is an ugly solution but i needed it so the service worker will
 // detect the change in the root url '/', but also serve it once offline
