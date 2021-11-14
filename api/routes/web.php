a14d104c327dbedff9b19ff4bcdc1c64a508b8a0
Marko <marko.cepo@marker>
2021-11-14 10:14:27 +0100

    removed unsued route

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/guide', 'WebController@guide');
 Route::get('/', 'WebController@index');
 
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
