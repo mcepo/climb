c46b57921bf5e8fb36a2f9f01952495a9a435efa
Marko <marko.cepo@marker>
2021-12-02 21:53:50 +0100

    remove unused info dialog

--- spa/src/views/Dashboard.vue
+++ spa/src/views/Dashboard.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app>
     <nav-bar></nav-bar>
-    <info-dialog></info-dialog>
 
     <v-main style='padding:0px' v-if='loadMain'>
       <drawing-actionbar></drawing-actionbar>
@@ -19,7 +18,6 @@ import Vue from 'vue'
 import DrawingActionbar from '../components/common/DrawingActionbar.vue'
 import NotificationSnackbar from '../components/common/NotificationSnackbar.vue'
 import NavBar from '../components/common/NavBar.vue'
-import InfoDialog from '../components/common/InfoDialog.vue'
 import '../plugins/leaflet'
 
 import drawers from '../services/drawer.service'
@@ -29,8 +27,7 @@ export default Vue.extend({
   components: {
     DrawingActionbar,
     NavBar,
-    NotificationSnackbar,
-    InfoDialog
+    NotificationSnackbar
   },
   computed: {
     loadMain () {
